36628c1ecc39b48c215ccc71e772ba7575504586
Sébastien <Sbastien@Sonic>
2014-11-05 22:36:02 +0100

    Ajout d'une route pour récupérer les capteurs

--- api/server.js
+++ api/server.js
@@ -49,9 +49,9 @@ router.use(function(req, res, next) {
     next();
 });
 
-var curut = router.route('/temperatures/records');
+var records = router.route('/temperatures/records');
 
-curut.get(function(req,res){
+records.get(function(req,res){
 
     req.getConnection(function(err,conn){
 
@@ -66,10 +66,29 @@ curut.get(function(req,res){
 
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(rows, null, 3));
-
-            // res.render('user',{title:"RESTful Crud Example",data:rows});
          });
     });
+});
+
+var sensors = router.route('/temperatures/sensors');
+
+sensors.get(function(req,res){
+	
+	req.getConnection(function(err,conn){
+		
+		if (err) return next("Cannot Connect");
+		
+		var query = conn.query('SELECT * FROM sensors',function(err,rows){
+			
+			if(err){
+				console.log(err);
+				return next("Mysql error, check your query");
+			}
+			
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(rows, null, 3));
+		});
+	});
 });
 
 //now we need to apply our router here
