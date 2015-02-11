aa71e378b9eada6125ef78a009d33dcff1d82116
Seb <seb@seb.fr>
2015-02-11 13:28:14 +0100

    Test

--- scripts/toctoc.sh
+++ scripts/toctoc.sh
@@ -5,7 +5,7 @@ password=$2
 
 curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d' | while read arg1; do
    REGEX="([a-zA-Z]+)rmement.*"
-
+   echo $arg1
    if [[ $arg1 =~ $REGEX ]]
    then
       if [ "${BASH_REMATCH[1]}" = "A" ]
