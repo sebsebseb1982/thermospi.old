9dabd410b4df7a4fabaca2d2bcde8644d8b3541a
Seb <seb@seb.fr>
2015-02-12 07:56:51 +0100

    Factorisation

--- api/server.js
+++ api/server.js
@@ -1,31 +1,31 @@
-var express  = require('express'),
-    path     = require('path'),
+var express = require('express'),
+    path = require('path'),
     bodyParser = require('body-parser'),
     app = express(),
     expressValidator = require('express-validator');
 
 
 /*Set EJS template Engine*/
-app.set('views','./views');
-app.set('view engine','ejs');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})); //support x-www-form-urlencoded
 app.use(bodyParser.json());
 app.use(expressValidator());
 
 /*MySql connection*/
-var connection  = require('express-myconnection'),
+var connection = require('express-myconnection'),
     mysql = require('mysql');
 
 app.use(
-    connection(mysql,{
-        host     : 'localhost',
-        user     : 'seb',
-        password : 'seb',
-        database : 'temperatures',
-        debug    : false //set true if you wanna see debug logger
-    },'request')
+    connection(mysql, {
+        host: 'localhost',
+        user: 'seb',
+        password: 'seb',
+        database: 'temperatures',
+        debug: false //set true if you wanna see debug logger
+    }, 'request')
 );
 
 //RESTful route
@@ -33,57 +33,57 @@ var router = express.Router();
 
 
 /*------------------------------------------------------
-*  This is router middleware,invoked everytime
-*  we hit url /api and anything after /api
-*  like /api/user , /api/user/7
-*  we can use this for doing validation,authetication
-*  for every route started with /api
---------------------------------------------------------*/
-router.use(function(req, res, next) {
+ *  This is router middleware,invoked everytime
+ *  we hit url /api and anything after /api
+ *  like /api/user , /api/user/7
+ *  we can use this for doing validation,authetication
+ *  for every route started with /api
+ --------------------------------------------------------*/
+router.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
 var records = router.route('/records');
 
-records.get(function(req,res){
+var querySQL = function (err, conn, sql) {
+    if (err) return next("Cannot Connect");
 
-    req.getConnection(function(err,conn){
+    var query = conn.query(sql, function (err, rows) {
+
+        if (err) {
+            console.log(err);
+            return next("Mysql error, check your query");
+        }
+
+        return rows;
+    });
+};
+
+records.get(function (req, res) {
+    var sql = 'SELECT * FROM records WHERE date >= ( CURDATE() - INTERVAL 1 DAY )';
+    req.getConnection(querySQL(err, conn, sql));
+});
+
+var sensors = router.route('/sensors');
+
+sensors.get(function (req, res) {
+
+    req.getConnection(function (err, conn) {
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query('SELECT * FROM records WHERE date >= ( CURDATE() - INTERVAL 1 DAY )',function(err,rows){
+        var query = conn.query('SELECT * FROM sensors', function (err, rows) {
 
-            if(err){
+            if (err) {
                 console.log(err);
                 return next("Mysql error, check your query");
             }
 
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(rows, null, 3));
-         });
+        });
     });
-});
-
-var sensors = router.route('/sensors');
-
-sensors.get(function(req,res){
-	
-	req.getConnection(function(err,conn){
-		
-		if (err) return next("Cannot Connect");
-		
-		var query = conn.query('SELECT * FROM sensors',function(err,rows){
-			
-			if(err){
-				console.log(err);
-				return next("Mysql error, check your query");
-			}
-			
-			res.setHeader('Content-Type', 'application/json');
-			res.end(JSON.stringify(rows, null, 3));
-		});
-	});
 });
 
 /* ====================================================================================================== */
@@ -92,49 +92,49 @@ sensors.get(function(req,res){
 
 var setPoints = router.route('/setpoints');
 
-setPoints.get(function(req,res){
-	
-	req.getConnection(function(err,conn){
-		
-		if (err) return next("Cannot Connect");
-		
-		var query = conn.query('SELECT * FROM setpoints WHERE date >= ( CURDATE() - INTERVAL 1 DAY )',function(err,rows){
-			
-			if(err){
-				console.log(err);
-				return next("Mysql error, check your query");
-			}
-			
-			res.setHeader('Content-Type', 'application/json');
-			res.end(JSON.stringify(rows, null, 3));
-		});
-	});
+setPoints.get(function (req, res) {
+
+    req.getConnection(function (err, conn) {
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query('SELECT * FROM setpoints WHERE date >= ( CURDATE() - INTERVAL 1 DAY )', function (err, rows) {
+
+            if (err) {
+                console.log(err);
+                return next("Mysql error, check your query");
+            }
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(rows, null, 3));
+        });
+    });
 });
 
-setPoints.post(function(req,res){
-	req.assert('value','Setpoint value is required').notEmpty();
-	var errors = req.validationErrors();
-	if(errors){
-		res.status(422).json(errors);
-		return;
-	}
-	
-	var exec = require('child_process').exec;
-	exec('~/thermospi/scripts/setPoint.sh ' + req.query.value, function(error, stdout, stderr) {
-	    console.log('stdout: ', stdout);
-	    console.log('stderr: ', stderr);
-	    if (error !== null) {
-	        console.log('exec error: ', error);
-	    }
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(stdout);
-	});
+setPoints.post(function (req, res) {
+    req.assert('value', 'Setpoint value is required').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        res.status(422).json(errors);
+        return;
+    }
+
+    var exec = require('child_process').exec;
+    exec('~/thermospi/scripts/setPoint.sh ' + req.query.value, function (error, stdout, stderr) {
+        console.log('stdout: ', stdout);
+        console.log('stderr: ', stderr);
+        if (error !== null) {
+            console.log('exec error: ', error);
+        }
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(stdout);
+    });
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
@@ -142,8 +142,8 @@ app.use(function(req, res, next) {
 app.use('/api', router);
 
 //start Server
-var server = app.listen(3000,function(){
+var server = app.listen(3000, function () {
 
-   console.log("Listening to port %s",server.address().port);
+    console.log("Listening to port %s", server.address().port);
 
 });
