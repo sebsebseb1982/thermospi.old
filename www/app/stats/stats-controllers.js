e25509120be451c8c8c9bbfb4a38d3f0609caa6f
sebsebseb <seb@seb.fr>
2016-01-12 17:10:59 +0100

    Remise en forme des services

--- www/app/stats/stats-controllers.js
+++ www/app/stats/stats-controllers.js
@@ -15,44 +15,48 @@ angular
             'temperatureSeries',
             'statusSeries',
             function ($scope, temperatureSeries, statusSeries) {
-                $scope.termperaturesConfig = {
-                    options: {
-                        chart: {
-                            type: 'spline'
-                        },
-                        tooltip: {
-                            style: {
-                                padding: 10,
-                                fontWeight: 'bold'
-                            }
-                        }
-                    },
 
-                    //Series object (optional) - a list of series using normal highcharts series options.
-                    series: temperatureSeries.get(),
-                    //Title configuration (optional)
-                    title: {
-                        text: 'Températures'
-                    },
-                    //Boolean to control showng loading status on chart (optional)
-                    loading: false,
-                    //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-                    //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
-                    xAxis: {
-                        type: 'datetime',
-                        plotBands: statusSeries.get()
-                    },
-                    //Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
-                    useHighStocks: false,
-                    //size (optional) if left out the chart will default to size of the div or something sensible.
-                    size: {
-                        height: 500
-                    },
-                    //function (optional)
-                    func: function (chart) {
-                        //setup some logic for the chart
-                    }
-                };
+                temperatureSeries.get().then(function (promisedTemperatureSeries) {
+
+                    $scope.termperaturesConfig = {
+                        options: {
+                            chart: {
+                                type: 'spline'
+                            },
+                            tooltip: {
+                                style: {
+                                    padding: 10,
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
+
+                        //Series object (optional) - a list of series using normal highcharts series options.
+                        series: promisedTemperatureSeries,
+                        //Title configuration (optional)
+                        title: {
+                            text: 'Températures'
+                        },
+                        //Boolean to control showng loading status on chart (optional)
+                        loading: false,
+                        //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+                        //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+                        xAxis: {
+                            type: 'datetime',
+                            plotBands: statusSeries.get()
+                        },
+                        //Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
+                        useHighStocks: false,
+                        //size (optional) if left out the chart will default to size of the div or something sensible.
+                        size: {
+                            height: 500
+                        },
+                        //function (optional)
+                        func: function (chart) {
+                            //setup some logic for the chart
+                        }
+                    };
+                });
             }
         ]
     );
